3087ad009bca9aaa5f5d243bc73442d7f0fe7f4a
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-04-20 18:24:54 +0700

    lang locale

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -72,14 +72,14 @@
                         @endguest
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                {{ Config::get('languages')[App::getLocale()] }}
+                              <span class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            @foreach (Config::get('languages') as $lang => $language)
-                                @if ($lang != App::getLocale())
-                                        <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
-                                @endif
-                            @endforeach
+                               @foreach (Config::get('languages') as $lang => $language)
+                                    @if ($lang != App::getLocale())
+                                         <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span> {{$language['display']}}</a>
+                                   @endif
+                               @endforeach
                             </div>
                         </li>
                     </ul>
